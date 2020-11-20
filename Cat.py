98b9cb988b751f8b12b23c67f491ecafad766407
Anastasia-Treshchina <74378727+Anastasia-Treshchina@users.noreply.github.com>
2020-11-20 17:21:29 +0400

    Update Cat.py

--- Cat.py
+++ Cat.py
@@ -2,14 +2,14 @@ from pet import Cat
 
 cat1 = Cat("Барон", "Мальчик", "2 года")
 
-print("Первого кота зовут ", cat1.getName())
-print("Пол Барона ", cat1.getGender())
-print("Возраст Барона = ", cat1.getAge())
+print("Первого кота зовут", cat1.getName())
+print("Пол кота", cat1.getName(), cat1.getGender())
+print("Возраст кота =",cat1.getName(), cat1.getAge())
 
 print("---------")
 
 cat2 = Cat("Сэм", "Мальчик", "2 года")
 
-print("Второго кота зовут ", cat2.getName())
-print("Пол Сэма ", cat2.getGender())
-print("Возраст Сэма = ", cat2.getAge())
+print("Второго кота зовут", cat2.getName())
+print("Пол кота", cat2.getName(), cat2.getGender())
+print("Возраст кота =", cat2.getName(), cat2.getAge())
